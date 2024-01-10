a056fdd1639e34b52ad0c40f85d44d54065b255f
vdolhushyn <vetajika@gmail.com>
2024-01-10 13:34:20 +0200

    message not neccessery

--- model/message_model.py
+++ model/message_model.py
@@ -6,7 +6,7 @@ from model.base_model import BaseModel
 class MessageModel(BaseModel):
     chat_id_from = BigIntegerField()
     chat_id_to = BigIntegerField()
-    message = TextField()
+    message = TextField(null=True)
     date = DateTimeField()
 
     class Meta:
